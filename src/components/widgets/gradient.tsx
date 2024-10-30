3c2c10140c2b37d565ec1deeb7d163253735eb30
James Pike <jamesandrewpike@gmail.com>
2024-10-30 10:51:07 -0400

    commit

--- src/components/widgets/gradient.tsx
+++ src/components/widgets/gradient.tsx
@@ -5,7 +5,7 @@ export const Gradient = component$((props: QwikIntrinsicElements['div']) => {
   return (
     <div
       {...props}
-      class={`${props.className ?? ''} absolute inset-0 bottom-0 h-[90vh] rounded-lg ring-1 ring-inset ring-black/5 bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]`}
+      class={`${props.className ?? ''} absolute inset-2 bottom-0 h-[90vh]  ring-1 ring-inset ring-black/5 bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff] sm:bg-[linear-gradient(145deg,var(--tw-gradient-stops))]`}
     
     
     />
