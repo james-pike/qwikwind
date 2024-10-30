fa59e2e48ecd43a6f97fe9d7f0efc0f683a90dbb
James Pike <jamesandrewpike@gmail.com>
2024-10-30 16:52:47 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import { Image } from "@unpic/qwik";
 
-const coverImage = "/src/assets/images/dvp.jpg";
+const coverImage = "/src/assets/images/hero.png";
 
 
 export default component$(() => {
